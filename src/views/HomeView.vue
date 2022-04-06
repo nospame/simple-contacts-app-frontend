e0d5c138aab9252ea2a745360275ee1ac9af0d2f
nospame <mapson.evan@gmail.com>
2022-04-06 11:37:50 -0700

    add create and show contact actions

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,7 +4,9 @@ export default {
   data: function () {
     return {
       message: "Welcome to the Contacts App!",
-      contacts: []
+      contacts: [],
+      newContact: {},
+      currentContact: {}
     };
   },
   created: function () {
@@ -18,6 +20,18 @@ export default {
           this.contacts = response.data;
         })
 
+    },
+    createContact: function () {
+      axios.post('/contacts', this.newContact)
+        .then(response => {
+          console.log(response.data);
+          this.contacts.push(response.data);
+        })
+    },
+    showContact: function (contact) {
+      this.currentContact = contact;
+      console.log(this.currentContact);
+      document.querySelector("#contact-details").showModal();
     }
   },
 };
@@ -26,10 +40,44 @@ export default {
 <template>
   <div class="home">
     <h1>{{ message }}</h1>
+    <p>
+      First Name:
+      <input v-model="newContact.first_name" />
+      <br />Last Name:
+      <input v-model="newContact.last_name" />
+      <br />Email:
+      <input v-model="newContact.email" />
+      <br />Phone Number:
+      <input v-model="newContact.phone_number" />
+      <br />Image URL
+      <input v-model="newContact.image" />
+      <br />
+      <button v-on:click="createContact()">Add Contact</button>
+    </p>
     <div v-for="contact in contacts">
-      <p>{{ contact.first_name }} {{ contact.last_name }}</p>
+      <p>
+        {{ contact.first_name }} {{ contact.last_name }}
+        <br />
+        <button v-on:click="showContact(contact)">Details</button>
+      </p>
     </div>
+    <dialog id="contact-details">
+      <form>
+        <img v-bind:src="currentContact.image" />
+        <h2>{{ currentContact.first_name }} {{ currentContact.last_name }}</h2>
+        <p>
+          {{ currentContact.phone_number }}
+          <br />
+          {{ currentContact.email }}
+        </p>
+        <button>Close</button>
+      </form>
+    </dialog>
   </div>
 </template>
 
-<style></style>
+<style>
+img {
+  width: 120px;
+}
+</style>
