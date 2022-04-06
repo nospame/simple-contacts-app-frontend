632b5163e971a56f812e6c73e36cb2136564f0b1
nospame <mapson.evan@gmail.com>
2022-04-06 12:14:57 -0700

    add update and destroy contact actions

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -32,6 +32,20 @@ export default {
       this.currentContact = contact;
       console.log(this.currentContact);
       document.querySelector("#contact-details").showModal();
+    },
+    updateContact: function (contact) {
+      axios.patch(`/contacts/${contact.id}.json`, this.currentContact)
+        .then(response => {
+          console.log(response.data);
+        })
+    },
+    deleteContact: function (contact) {
+      console.log(contact.id);
+      axios.delete(`/contacts/${contact.id}.json`)
+        .then(response => {
+          console.log(response.data);
+          this.contacts.splice(this.contacts.indexOf(contact), 1);
+        })
     }
   },
 };
@@ -54,22 +68,37 @@ export default {
       <br />
       <button v-on:click="createContact()">Add Contact</button>
     </p>
-    <div v-for="contact in contacts">
+    <div v-for="contact in contacts" id="contacts-list" v-bind:key="contact.id">
       <p>
+        <img v-bind:src="contact.image" />
+        <br />
         {{ contact.first_name }} {{ contact.last_name }}
         <br />
         <button v-on:click="showContact(contact)">Details</button>
       </p>
     </div>
     <dialog id="contact-details">
-      <form>
+      <form method="dialog">
         <img v-bind:src="currentContact.image" />
         <h2>{{ currentContact.first_name }} {{ currentContact.last_name }}</h2>
         <p>
-          {{ currentContact.phone_number }}
+          First Name:
+          <input v-model="currentContact.first_name" />
+          <br />Last Name:
+          <input v-model="currentContact.last_name" />
+          <br />Email:
+          <input v-model="currentContact.email" />
+          <br />Phone Number:
+          <input v-model="currentContact.phone_number" />
+          <br />Image URL:
+          <input v-model="currentContact.image" />
           <br />
-          {{ currentContact.email }}
         </p>
+        <p>
+          <button v-on:click="updateContact(currentContact)">Update</button>
+          <button v-on:click="deleteContact(currentContact)">Delete Contact</button>
+        </p>
+
         <button>Close</button>
       </form>
     </dialog>
@@ -77,7 +106,11 @@ export default {
 </template>
 
 <style>
-img {
-  width: 120px;
+#contacts-list img {
+  width: 100px;
+}
+
+#contact-details img {
+  width: 300px;
 }
 </style>
